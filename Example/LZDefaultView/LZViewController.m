cb6ad6252e75955fcfb85dedf8eec817b0209cea
DarvwinHacker <zhuliu001@users.noreply.github.com>
2020-01-22 12:28:00 +0800

    事件响应者链条探究

--- Example/LZDefaultView/LZViewController.m
+++ Example/LZDefaultView/LZViewController.m
@@ -8,6 +8,7 @@
 
 #import "LZViewController.h"
 #import <LZDefaultView/LZDefaultView.h>
+#import <Masonry/Masonry.h>
 
 @interface LZViewController ()
 
@@ -46,6 +47,47 @@
     self.defaultView.secondLabelBlock = ^(UILabel * _Nonnull secondLabel) {
         NSLog(@"%@",secondLabel);
     };
+}
+
+//事件响应者链
+- (void)eventLinks
+{
+    UIButton * testButton1 = [UIButton buttonWithType:UIButtonTypeSystem];
+    testButton1.backgroundColor = [UIColor redColor];
+    [testButton1 addTarget:self action:@selector(testButton1Action:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:testButton1];
+    [testButton1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.top.mas_equalTo(100);
+        make.width.height.mas_equalTo(50);
+    }];
+    
+    UIButton * testButton2 = [UIButton buttonWithType:UIButtonTypeSystem];
+    testButton2.backgroundColor = [UIColor yellowColor];
+    [testButton2 addTarget:self action:@selector(testButton2Action:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:testButton2];
+    [testButton2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.top.mas_equalTo(100);
+        make.width.height.mas_equalTo(50);
+    }];
+    testButton2.userInteractionEnabled = NO;
+    
+    UIImageView * testImageView = [[UIImageView alloc]init];
+    testImageView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:testImageView];
+    [testImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.top.mas_equalTo(100);
+        make.width.height.mas_equalTo(50);
+    }];
+}
+
+- (void)testButton1Action:(UIButton *)button
+{
+    NSLog(@"button1Action");
+}
+
+- (void)testButton2Action:(UIButton *)button
+{
+    NSLog(@"button2Action");
 }
 
 @end
